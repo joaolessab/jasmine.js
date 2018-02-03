369d78ba4f034df7884bc2531a9fee23bbe8b96e
jvlessa <jvcadastro@gmail.com>
2018-02-03 16:51:26 -0200

    Little refactor with switch

--- src/Animal.js
+++ src/Animal.js
@@ -1,10 +1,8 @@
 function Animal(){} //Constructor
 
 Animal.prototype.numLegs = function(){
-	if (this.kind == "insect"){
-		return 6;
-	}
-	else if (this.kind == "spider"){
-		return 8;
-	}
+	switch (this.kind){
+		case "insect": return 6;
+		case "spider": return 8;
+	};
 };
