7d6c38b3e9e327c4bd39caa8f33722f6841d178d
jvlessa <jvcadastro@gmail.com>
2018-02-03 17:40:17 -0200

    [Soccer Player] - to Match regular expressions

--- spec/SoccerPlayerSpec.js
+++ spec/SoccerPlayerSpec.js
@@ -1,14 +1,23 @@
 describe("SoccerPlayer", function(){
-	//Goalkeeper
+	//Testing position
 	it("Should be a Goalkeeper", function(){
 		var player = new SoccerPlayer();
 		player.position = "goalkeeper";
 		expect(player.isGoalKeeper()).toBe(true); //Boolean
 	});
 
+	//Testing number of the shirt
 	it("Goalkeeper should has the shirt number 12", function(){
 		var player = new SoccerPlayer();
 		player.position = "goalkeeper";
 		expect(player.shirtNumber()).toEqual(12); //To Equal (could use toBe)
+	});
+
+	//Testing country star - Cristiano Ronaldo
+	it("Name of the country star passed regular expression", function(){
+		var player = new SoccerPlayer();
+		player.countryStar = "portugal";
+		expect(player.starName()).toMatch("naldo"); //Regular Expressions
+		expect(player.starName()).toMatch(/naldo/); //Regular Expressions
 	});
 });
--- src/SoccerPlayer.js
+++ src/SoccerPlayer.js
@@ -9,3 +9,12 @@ SoccerPlayer.prototype.shirtNumber = function(){
 	if (this.position == "goalkeeper")
 		return 12;
 };
+
+SoccerPlayer.prototype.starName = function(){
+	switch (this.countryStar){
+		case "portugal": return "Cristiano Ronaldo";
+		case "argentina": return "Lionel Messi";
+		case "brazil": return "Neymar Júnior";
+		case "france": return "Paul Pogba";
+	};
+};
